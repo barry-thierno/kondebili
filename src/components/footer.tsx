32d418e79546d923e01ab6b3906c9bd8f946b914
barry-thierno <thsaidbarry@gmail.com>
2019-12-17 00:43:11 +0100

    feat: construire les template responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,7 @@
 import * as React from "react"
 import "./footer.scss"
 const Footer = () => {
-  return (
-    <footer>
-      © {new Date().getFullYear()}, Built with {`  `}
-      <a href="https://www.gatsbyjs.org"> Gatsby </a> by Kondebili team
-    </footer>
-  )
+  return <footer>© {new Date().getFullYear()}, Built by Kondebili team</footer>
 }
 
 export default Footer
